44e92cc381d2b52e1e8c282ead36aa21df52e1d7
Karim Oulad Chalha <herr.linux88@gmail.com>
2015-12-22 21:54:35 +0000

    changing pagination button height and enabling the sropdown menu button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 (function($) {
-  var has_child = $('#menu-main .menu-item-has-children');
-  var sub_menu  = $('#menu-main .dropdown-content');
+  var has_child = $('#menu-pages .menu-item-has-children');
+  var sub_menu  = $('#menu-pages .dropdown-content');
+  console.log(has_child);
   has_child.addClass('dropdown-button');
   for (var i=0; i < has_child.length; i++) {
-    $('#menu-main .dropdown-content:eq('+i+')').attr('id', 'dropdown'+i);
-    $('#menu-main .menu-item-has-children:eq('+i+')').attr('data-activates', 'dropdown'+i);
-    $('#menu-main .menu-item-has-children:eq('+i+')').attr('data-beloworigin', 'true');
-    $('#menu-main .menu-item-has-children:eq('+i+')').attr('data-hover', 'true');
+    console.log(i);
+    $('#menu-pages .dropdown-content:eq('+i+')').attr('id', 'dropdown'+i);
+    $('#menu-pages .menu-item-has-children:eq('+i+')').attr('data-activates', 'dropdown'+i);
+    $('#menu-pages .menu-item-has-children:eq('+i+')').attr('data-beloworigin', 'true');
+    $('#menu-pages .menu-item-has-children:eq('+i+')').attr('data-hover', 'true');
   }
+
 /*
   $('img').addClass('materialboxed');
   $('img.avatar').removeClass('materialboxed');
@@ -15,7 +18,7 @@
   $('textarea').addClass('materialize-textarea');
   $('input[type="submit"]').addClass('btn waves-effect blue');
   $('textarea#comment').attr('placeholder', 'Your comment here...');
-  $('.page-numbers, .comment-reply-link, #menu-main .menu-item, #menu-main li a').addClass('waves-effect');
+  $('.page-numbers, .comment-reply-link, #menu-pages .menu-item, #menu-pages li a').addClass('waves-effect');
   $('.widget_search input#s').addClass('white');
   //initializing Mobile Nav
   $(".button-collapse").sideNav();
